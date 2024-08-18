c13cdf4571c4863487fc058de89b9272c0cfb5a9
kiganaoakuma <daltonhberg@icloud.com>
2024-08-18 16:15:54 -0700

    config comment for tsx and typescript

--- lua/artyrc/plugins/comment.lua
+++ lua/artyrc/plugins/comment.lua
@@ -11,6 +11,8 @@ return {
 		local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
 
 		-- enable comment
-		comment.setup({})
+		comment.setup({
+			pre_hook = ts_context_commentstring.create_pre_hook(),
+		})
 	end,
 }
